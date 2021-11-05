75375f0653f86dd3ea99f6d72da2bd9cf924a0eb
LeeProgrammer <jaeho010718@gmail.com>
2021-11-05 16:19:35 +0900

    Basic Setting

---
+++ Moit/Moit/MoitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoitApp.swift
+//  Moit
+//
+//  Created by Lee Jaeho on 2021/11/05.
+//
+
+import SwiftUI
+
+@main
+struct MoitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
